eff7563ef1d34ee769f37892d09392464168d79f
Adrian Tan <adrian.tan@hec.edu>
2022-11-15 21:35:15 +0000

    unit test train length

---
+++ unit_tests/test_length.py
@@ -0,0 +1,16 @@
+import os
+import pandas as pd
+
+def test_xySameLength():
+    """
+    Count train image count and trainLabels file count
+    
+    FIX: update paths later 
+    """
+    trainLabelPath = '../hfactory_magic_folders/colas_data_challenge/computer_vision_challenge/dataset/labels_train.csv'
+    imageTrainPath = '../hfactory_magic_folders/colas_data_challenge/computer_vision_challenge/dataset/train/'
+
+    trainLabels = pd.read_csv(trainLabelPath)
+    imageLen = len([file for file in os.listdir(imageTrainPath)])
+    assert len(trainLabels.filename) == imageLen
+    
