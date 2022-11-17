18d679a31ce19491e57b4acc88209b58f93c9fbb
Augustin de la Brosse <augustin.de-la-brosse@hec.edu>
2022-11-17 16:48:48 +0000

    rm gitignore files in modelResNet & imageExtraction

--- modelResNet/config.py
+++
@@ -1,5 +0,0 @@
-lr = 0.0001
-epochs = 20
-batch_size = 16
-trainLabelPath = '../../hfactory_magic_folders/colas_data_challenge/computer_vision_challenge/dataset/labels_train.csv'
-imageTrainPath = '../../hfactory_magic_folders/colas_data_challenge/computer_vision_challenge/dataset/train/'
