7e809b6d94c872f73b3ff90d3d5d89f3e5d8f24c
Lea Chader <lea.chader@hec.edu>
2022-11-17 01:46:45 +0000

    Add more tests
    Signed-off-by: Lea Chader <lea.chader@hec.edu>

--- unit_tests/ExtractionAirports/test_extraction_airports.py
+++ unit_tests/ExtractionAirports/test_extraction_airports.py
@@ -1,4 +1,5 @@
 """Unit tests for extraction airport functions"""
+
 import sys
 import json
 sys.path.insert(0, "../src/Extraction_Airports/")
@@ -8,8 +9,10 @@ import extraction_airports
 path_conf = './Params/config_test.json'
 conf = json.load(open(path_conf, 'r'))
 
+
 def test_get_edges_photos():
     """Test the extraction of orthophoto edges.
+
     Steps:
         Create a class Extraction Airports
         Get the dataframe containing the geometry of photos.
@@ -36,49 +39,159 @@ def test_get_edges_photos():
     assert min_lon == 670000.0
     assert max_lon == 675000.0
     assert len(df) == 3
-    
-    def test_get_edges_intersection():
-        """Test the extraction of orthophoto edges.
-        Steps:
-            Create a class Extraction Airports
-            Get the dataframe containing the geometry of photos.
-            Note: Do the test on the dataframe containing
-            one orthophoto per airport.
-            Get the geometry of photos and the geometry
-            of the intersection of the airport and the photo.
-            The geometry of photos and intersection are polygons.
-            Get the edges of the intersection.
-        Output:
-            max_1: 8191
-            max_2: 7781
-            min_1: 5077
-            min_2: 4313
-            poly_crop: [(5186, 4448), (5782, 4319),
-                        (6271, 4368), (7481, 4313),
-                        (7615, 4389), (8160, 4448),
-                        (8191, 5483), (8116, 5522),
-                        (8060, 6192), (7945, 6206),
-                        (7376, 7350), (7127, 7228),
-                        (6932, 7781), (6618, 7674),
-                        (5930, 7498), (6230, 5756),
-                        (5077, 5572), (5186, 4448)]
-        """
-        img_shape = (25000, 25000)
-        extraction_airports_class = extraction_airports.Extractionairports(conf, img_shape)
-        df, _, _ = extraction_airports_class.read_create_input()
-        poly_boundaries = df.iloc[0]['geometry_photos']
-        poly_intersection = df.iloc[0]['geometry_intersection']
-        max_1, max_2, min_1, min_2, poly_crop = extraction_airports_class.get_edges_intersection(poly_intersection, poly_boundaries)
-        assert max_1 == 8191
-        assert max_2 == 7781
-        assert min_1 == 5077
-        assert min_2 == 4313
-        assert poly_crop == [(5186, 4448), (5782, 4319),
-                             (6271, 4368), (7481, 4313),
-                             (7615, 4389), (8160, 4448),
-                             (8191, 5483), (8116, 5522),
-                             (8060, 6192), (7945, 6206),
-                             (7376, 7350), (7127, 7228),
-                             (6932, 7781), (6618, 7674),
-                             (5930, 7498), (6230, 5756),
-                             (5077, 5572), (5186, 4448)]
+    assert df.iloc[0]["TOPONYME"] == "aérodrome de chelles-le pin"
+
+def test_get_edges_intersection():
+    """Test the extraction of orthophoto edges.
+
+    Steps:
+        Create a class Extraction Airports
+        Get the dataframe containing the geometry of photos.
+        Note: Do the test on the dataframe containing
+        one orthophoto per airport.
+        Get the geometry of photos and the geometry
+        of the intersection of the airport and the photo.
+        The geometry of photos and intersection are polygons.
+        Get the edges of the intersection.
+    Output:
+        max_1: 8190
+        max_2: 7779
+        min_1: 5075
+        min_2: 4310
+        poly_crop: see assert below
+    """
+    img_shape = (25000, 25000)
+    extraction_airports_class = extraction_airports.Extractionairports(conf, img_shape)
+    df, _, _ = extraction_airports_class.read_create_input()
+    poly_boundaries = df.iloc[0]['geometry_photos']
+    poly_intersection = df.iloc[0]['geometry_intersection']
+    max_1, max_2, min_1, min_2, poly_crop = extraction_airports_class.get_edges_intersection(poly_intersection, poly_boundaries)
+    print(poly_crop)
+    assert max_1 == 8190
+    assert max_2 == 7779
+    assert min_1 == 5075
+    assert min_2 == 4310
+    assert poly_crop == [(5185, 4445), (5780, 4315),
+                         (6270, 4365), (7480, 4310),
+                         (7615, 4385), (8160, 4445),
+                         (8190, 5480), (8115, 5520),
+                         (8060, 6190), (7945, 6205),
+                         (7375, 7350), (7124, 7224),
+                         (6930, 7779), (6615, 7670),
+                         (5930, 7495), (6230, 5755),
+                         (5075, 5570), (5185, 4445)]
+
+
+def test_get_cropping_coordinates():
+    """Test the extraction of cropping coordinates.
+
+    Steps:
+        Create a class Extraction Airports
+        Get the dataframe containing the geometry of photos.
+        Note: Do the test on the dataframe containing
+        one orthophoto per airport.
+        Get the geometry of photos and the geometry
+        of the intersection of the airport and the photo.
+        The geometry of photos and intersection are polygons.
+        Get the cropping coordinates.
+    Output:
+        up_poly_crop: see assert below
+        f_min_1: -134911
+        f_max_1: 65730
+        min_2: -31051
+        max_2: 149063
+    """
+    img_shape = (25000, 25000)
+    extraction_airports_class = extraction_airports.Extractionairports(conf, img_shape)
+    df, _, _ = extraction_airports_class.read_create_input()
+    poly_boundaries = df.iloc[0]['geometry_photos']
+    poly_intersection = df.iloc[0]['geometry_intersection']
+    up_poly_crop, f_min_1, f_max_1, min_2, max_2 = extraction_airports_class.get_cropping_coordinates(
+        poly_intersection, poly_boundaries)
+    assert up_poly_crop == [(0, 0), (180114, 0),
+                            (180114, 200641),
+                            (0, 200641), (0, 0)]
+    assert f_min_1 == -134911
+    assert f_max_1 == 65730
+    assert min_2 == -31051
+    assert max_2 == 149063
+
+def test_get_photos_edges_multi():
+    """Test the extraction of edges of multiple images merged.
+
+    Steps:
+        Create a class Extraction Airports
+        Get the dataframe containing the geometry of photos.
+        Note: Do the test on the dataframe containing
+        sevral orthophotos per airport.
+        Get the geometry of photos.
+        The geometry of photos are polygons.
+        Get the edges for the multiphoto.
+    Output:
+        len(df_multi_photos): 5
+        len(dup_aero): 2
+        min_lat: 6876311.488950567
+        max_lat: 6880000.0
+        min_lon: 662829.5000000057
+        max_lon: 665000.0
+    """
+    img_shape = (25000, 25000)
+    extraction_airports_class = extraction_airports.Extractionairports(conf, img_shape)
+    _, df_multi_photos, dup_aero = extraction_airports_class.read_create_input()
+    df_aero_i = df_multi_photos[
+        df_multi_photos["TOPONYME"] == dup_aero[0]
+    ].reset_index(drop=True)
+    poly_boundaries = df_aero_i.iloc[0]["geometry_intersection"]
+    min_lat, max_lat, min_lon, max_lon = extraction_airports_class.get_edges_photos_multi(poly_boundaries)
+    assert len(df_multi_photos) == 5
+    assert len(dup_aero) == 2
+    assert min_lat == 6876311.488950567
+    assert max_lat == 6880000.0
+    assert min_lon == 662829.5000000057
+    assert max_lon == 665000.0
+
+
+def test_extract_airports():
+    """Test the extraction of airports function.
+
+    Steps:
+        Create a class Extraction Airports
+        Get the dataframe containing the geometry of photos.
+        Note: Do the test on the dataframe containing
+        one orthophoto per airport.
+        Get the geometry of photos, the input path
+        and the output path.
+    Output:
+        Should be None and function should run without issues.
+    """
+    img_shape = (25000, 25000)
+    extraction_airports_class = extraction_airports.Extractionairports(conf, img_shape)
+    df, _, _ = extraction_airports_class.read_create_input()
+    df = df.head(1)
+    path_input = conf["paths"]["bdortho_input_path"]
+    path_output = conf["paths"]["Outputs_path"] + conf["paths"]["airportextraction_file"]
+    output = extraction_airports_class.extract_airports(df, path_input, path_output)
+    assert output == None
+'''
+def test_extract_airports_multi():
+    """Test the extraction of airports function.
+
+    Steps:
+        Create a class Extraction Airports
+        Get the dataframe containing the geometry of photos.
+        Note: Do the test on the dataframe containing
+        sevral orthophotos per airport.
+        Get the geometry of photos, the input path
+        and the output path.
+    Output:
+        Should be None and function should run without issues.
+    """
+    img_shape = (25000, 25000)
+    extraction_airports_class = extraction_airports.Extractionairports(conf, img_shape)
+    _, df_multi_photos, dup_aero = extraction_airports_class.read_create_input()
+    dup_aero = dup_aero[0]
+    path_input = conf["paths"]["bdortho_input_path"]
+    path_output = conf["paths"]["Outputs_path"] + conf["paths"]["airportextraction_file"]
+    output = extraction_airports_class.extract_airports_multi(df_multi_photos, dup_aero, path_input, path_output)
+    assert output == None
+'''
