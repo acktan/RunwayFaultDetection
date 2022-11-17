dac57a2ce93792552643489e7ec864e8033269d4
Ines Benito <ines.benito@hec.edu>
2022-11-17 12:42:13 +0000

    Fixed bug path

--- unit_tests/Model/test_model.py
+++ unit_tests/Model/test_model.py
@@ -7,8 +7,8 @@ def test_xySameLength():
     """Ensure image count and train labels are the same length.
     """
     print(os.getcwd())
-    trainLabelPath = '../Inputs/Model/labels_train.csv'
-    imageTrainPath = '../Inputs/Model/train'
+    trainLabelPath = './Inputs/Model/labels_train.csv'
+    imageTrainPath = './Inputs/Model/train'
 
     trainLabels = pd.read_csv(trainLabelPath)
     imageLen = len([file for file in os.listdir(imageTrainPath)])
