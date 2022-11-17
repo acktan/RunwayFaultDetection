3afd0a16a4de860a66d7ee5b00df0aca0614866c
Augustin de la Brosse <augustin.de-la-brosse@hec.edu>
2022-11-17 00:46:10 +0000

    changed docstring to the prediction function

--- MobileNet/inference.py
+++ MobileNet/inference.py
@@ -13,20 +13,23 @@ warnings.filterwarnings('ignore')
 logging.getLogger("tensorflow").setLevel(logging.ERROR)
 
 
-def make_prediction(IDs, model, submission_name, path_to_saved_submissions_folder=PATH_TO_SAVED_SUBMISSIONS_FOLDER, testing_path=TESTING_PATH, thresh=PREDICTION_THRESH, save=True):
-    """Function that returns a tuple of normalized image array and labels array.
+def make_prediction(IDs, model, submission_name=None, path_to_saved_submissions_folder=PATH_TO_SAVED_SUBMISSIONS_FOLDER, testing_path=TESTING_PATH, thresh=PREDICTION_THRESH, save=True):
+    """Function that returns a dataframe with the predictions.
     Args:
-        filename: string representing path to image
-        label: 0/1 one-dimensional array of size N_LABELS, 
-        channels (int): number of channel, 3 by default when rgb,
-        img_size (int): image height and width,
-        max_delta (float), 
-        seed (tensor): a shape [2] Tensor, the seed to the random number generator. Must have dtype int32 or int64.
+        IDs (list): list of the ids of the tested images, 
+        model (keras.engine.sequential.Sequential), 
+        submission_name (None or str): name of the csv in case the user wants to save the predictions, 
+        path_to_saved_submissions_folder (str): path to the folder where the csv will be saved, 
+        testing_path (str): path to the tested images, 
+        thresh (float): probability value above which we predict a positive value, 
+        save (Boolean): indicates if the user wants to save as csv the predictions
     
     Returns:
-        image_augmented (tensorflow.python.data.ops.dataset_ops.ParallelMapDataset)
-        label (numpy.ndarray): array of labels
+        df (Pandas DataFrame): predictions
     """
+    if save==True:
+        assert type(submission_name) == str
+        
     for idx, ID in enumerate(IDs):
         img_path = os.path.join(testing_path, str(ID))
 
@@ -52,6 +55,7 @@ def make_prediction(IDs, model, submission_name, path_to_saved_submissions_folde
              'MISE EN DALLE']]
     
     if save==True:
+        # assert type(submission_name) == str
         df.to_csv(path_to_saved_submissions_folder + submission_name, index=False)
     
     return df
